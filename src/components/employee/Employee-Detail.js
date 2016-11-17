babc7146aa7d498f07891d5ffe3e104cd13d9d96
cwkummer <Chris.Kummer@state.ca.gov>
2016-11-16 20:00:55 -0800

    Fix full name variable.

--- src/components/employee/Employee-Detail.js
+++ src/components/employee/Employee-Detail.js
@@ -19,13 +19,13 @@ function EmployeeDetail(props) {
     'deskLocation': 'Desk Location',
     'mailStop': 'Mail Stop'
   };
-  
+
   return (
     <Table bordered className={Object.keys(props.employee).length ? '' : 'hidden'}>
       <thead>
         <tr className="table-header">
           <th colSpan="2">
-            <h2 className="pull-left">{props.employee.FullName}</h2>
+            <h2 className="pull-left">{props.employee.fullName}</h2>
             <Button className="pull-right" bsSize="large">Print</Button>
           </th>
         </tr>
