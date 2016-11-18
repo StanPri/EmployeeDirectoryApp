89159989a566486522f564cab12c2476c54f54e4
StanPri <spribytkovsky@yahoo.com>
2016-11-18 13:59:36 -0800

    Manager link done

--- src/components/employee/Employee-ManagerDetail.js
+++ src/components/employee/Employee-ManagerDetail.js
@@ -27,14 +27,21 @@ function ManagerDetail(props) {
           return (
             <tr key={key}>
               <td className="col-xs-3">{fields[key]}</td>
-              <td className="col-xs-9">
-                {Object.keys(props.manager).length ?
-                  ( (fields[key] === 'Name') ?
-                    <input type="button"
+                <td className="col-xs-9">
+                {//If manager exists
+                  (( Object.keys(props.manager).length) ?
+                  //If the key is name
+                    ( (fields[key] === 'Name') ?
+                      //then create a hyperlink
+                      <input type="button" className={'employee-manager-row'}
                       onClick={props.onClick}
-                      value={props.manager[key]} /> : props.manager[key] )
-                  : 'Not Available'}
-              </td>
+                      value={props.manager[key]} />
+                      //otherwise, just output the data
+                      :  props.manager[key])
+                  //or if manager doesn't exist then...
+                  : 'Not Available' )
+                }
+                </td>
             </tr>
           );
         })}
